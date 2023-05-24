dd6a8e359fefce85b56a63d26c56ee6c05afe927
Matheus Cantuária <matheuscantuaria.mc@gmail.com>
2023-05-24 11:03:03 -0300

    Exercicios

---
+++ ExerciciosFazer/1).js
@@ -0,0 +1,14 @@
+function numeros(valor1, valor2) {
+    var soma = valor1 + valor2
+    var subtracao = valor1 - valor2
+    var multiplicacao = valor1 * valor2
+    var divisao = valor1 / valor2
+
+    console.log(soma,
+        subtracao,
+        multiplicacao,
+        divisao)
+}
+
+numeros(20, 30)
+
