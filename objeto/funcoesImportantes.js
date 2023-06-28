de45711177f253208163b3b83df18e2e4e15cea8
Matheus Cantuária <98433110+Matheus0520@users.noreply.github.com>
2023-06-28 16:03:59 -0300

    ajuste

--- objeto/funcoesImportantes.js
+++ objeto/funcoesImportantes.js
@@ -8,6 +8,16 @@ console.log(Object.keys(pessoa)) // chave
 console.log(Object.values(pessoa)) // valor
 console.log(Object.entries(pessoa)) // entradas
 
-objectentries(pessoa).forEach(([chave, valor]) => {
+Object.entries(pessoa).forEach(([chave, valor]) => { 
     console.log(`${chave}: ${valor}`)
 })
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true, // coloca na lista
+    writable: false, // mudavel
+    value: '01/01/2019'
+})
+
+pessoa.dataNascimento = '01/01/2017' // como não é mudavel o valor não é alterado mesmo quando dado outro
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa))
